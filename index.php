b40ba24c87fc31b45f61f179d2d9c477e82c87b8
Victoria <victoria.sofianidou@gmail.com>
2020-10-19 16:51:34 +0200

    added error reporting code and whatshappening function

--- index.php
+++ index.php
@@ -1,5 +1,28 @@
 <?php
 declare(strict_types=1);
+//this line makes PHP behave in a more strict way
+
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
+
+//we are going to use session variables so we need to enable sessions
+session_start();
+
+function whatIsHappening()
+{
+    echo '<h2>$_GET</h2>';
+    var_dump($_GET);
+    echo '<h2>$_POST</h2>';
+    var_dump($_POST);
+    echo '<h2>$_COOKIE</h2>';
+    var_dump($_COOKIE);
+    echo '<h2>$_SESSION</h2>';
+    var_dump($_SESSION);
+}
+
+whatIsHappening(); // call function
+
 
 //include all your model files here
 require 'Model/User.php';
