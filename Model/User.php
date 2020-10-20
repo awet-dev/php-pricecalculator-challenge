44441aadd57a569a283e0e2b88f9818902043fc5
awet tesfay <whitezscope@gmail.com>
2020-10-20 10:28:09 +0200

    create the user class

--- Model/User.php
+++ Model/User.php
@@ -3,15 +3,52 @@ declare(strict_types=1);
 
 class User
 {
-    private $name;
+    private int $id;
+    private string $firstName;
+    private string $lastName;
+    private int $groupId;
+    private int $fixDiscount;
+    private int $variableDiscount;
 
-    public function __construct(string $name)
+
+    public function __construct(int $id, string $firstName, string $lastName, int $groupId, int $fixDiscount, int $variableDiscount)
     {
-        $this->name = $name;
+        $this->id = $id;
+        $this->firstName = $firstName;
+        $this->lastName = $lastName;
+        $this->groupId = $groupId;
+        $this->fixDiscount = $fixDiscount;
+        $this->variableDiscount = $variableDiscount;
     }
 
-    public function getName() : string
+    public function getId(): int
     {
-        return $this->name;
+        return $this->id;
     }
+
+    public function getFirstName(): string
+    {
+        return $this->firstName;
+    }
+
+    public function getLastName(): string
+    {
+        return $this->lastName;
+    }
+
+    public function getGroupId(): int
+    {
+        return $this->groupId;
+    }
+
+    public function getFixDiscount(): int
+    {
+        return $this->fixDiscount;
+    }
+
+    public function getVariableDiscount(): int
+    {
+        return $this->variableDiscount;
+    }
+
 }
