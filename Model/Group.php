f0826905a670a12bbc3bef5ae6310ae642de9e68
Victoria <victoria.sofianidou@gmail.com>
2020-10-20 11:01:05 +0200

    created Group class

--- Model/Group.php
+++ Model/Group.php
@@ -1,7 +1,55 @@
 <?php
 
+class Group {
 
-class Group
-{
+    private int $id;
+    private string $groupName;
+    private int $parentId;
+    private int $fixDiscount;
+    private int $varDiscount;
+
+    /**
+     * Group constructor.
+     * @param int $id
+     * @param string $groupName
+     * @param int $parentId
+     * @param int $fixDiscount
+     * @param int $varDiscount
+     */
+    public function __construct(int $id, string $groupName, int $parentId, int $fixDiscount, int $varDiscount)
+    {
+        $this->id = $id;
+        $this->groupName = $groupName;
+        $this->parentId = $parentId;
+        $this->fixDiscount = $fixDiscount;
+        $this->varDiscount = $varDiscount;
+    }
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function getGroupName(): string
+    {
+        return $this->groupName;
+    }
+
+    public function getParentId(): int
+    {
+        return $this->parentId;
+    }
+
+
+    public function getFixDiscount(): int
+    {
+        return $this->fixDiscount;
+    }
+
+    public function getVarDiscount(): int
+    {
+        return $this->varDiscount;
+    }
 
 }
+
