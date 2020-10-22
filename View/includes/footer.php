eeb9734ca1f462b518e0732ff8801c4b118ddcd0
awet tesfay <whitezscope@gmail.com>
2020-10-22 11:38:47 +0200

    modify the footer

--- View/includes/footer.php
+++ View/includes/footer.php
@@ -1,5 +1,5 @@
     <footer class="text-center my-3">
-        Yuri & Chris &copy; BeCode <?php echo date('Y')?>
+        Awet & Victoria &copy; BeCode <?php echo date('Y')?>
     </footer>
 </body>
 </html>
